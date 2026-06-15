dfdbb998ecebbd8976eb790199c912b997de66dc
nikhilj2887 <jjnikhil@gmail.com>
2026-06-15 22:19:04 +0530

    Add Google Analytics tracking component

---
+++ components/googleAnalytics.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
+export default function GoogleAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (typeof window === 'undefined' || !window.gtag) return;
+
+    const url =
+      pathname +
+      (searchParams.toString()
+        ? `?${searchParams.toString()}`
+        : '');
+
+    window.gtag('config', 'G-HJSDMZLT70', {
+      page_path: url,
+    });
+  }, [pathname, searchParams]);
+
+  return null;
+}
